059697509fbbc9cac55b3d2b99490796eb061485
vitkarpov <viktor.s.karpov@gmail.com>
2015-12-08 21:04:54 +0300

    adds order delivery bemjson

--- blocks/order-delivery/order-delivery.bemtree.js
+++ blocks/order-delivery/order-delivery.bemtree.js
@@ -30,6 +30,42 @@ block('order-delivery').content()(function() {
                     }
                 ]
             }
+        },
+        {
+            elem: 'item',
+            content: [
+                {
+                    block : 'radio',
+                    mods : { theme : 'parket', checked : true },
+                    text : 'Самовывоз из магазина — Бесплатно'
+                },
+                {
+                    elem: 'text',
+                    content: 'Забрать заказ можно из нашего магазина по адресу Нахимовский проспект, 24, выставка «Экспострой», павильон 1, место 34'
+                }
+            ]
+        },
+        {
+            elem: 'item',
+            content: [
+                {
+                    block : 'radio',
+                    mods : { theme : 'parket' },
+                    text : 'Курьерская доставка — 1 800₽ по Москве (за МКАД 20₽/км)'
+                },
+                {
+                    elem: 'text',
+                    content: 'По Москве 1800 рублей, теплых полов – 500 рублей. В регионы любой транспортной компанией на ваш выбор по ее тарифам'
+                }
+            ]
+        },
+        {
+            elem: 'item',
+            content: {
+                block: 'link',
+                url: '#',
+                content: 'Условия доставки'
+            }
         }
     ];
 })
