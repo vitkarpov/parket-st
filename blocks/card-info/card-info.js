515a0ef0907b1e13306d804ace1cd4038d1283bc
vitkarpov <viktor.s.karpov@gmail.com>
2015-12-12 16:34:26 +0300

    rm 5% change price

--- blocks/card-info/card-info.js
+++ blocks/card-info/card-info.js
@@ -3,7 +3,6 @@ modules.define('card-info', ['i-bem__dom'], function(provide, BEMDOM) {
 provide(BEMDOM.decl(this.name, {
     onSetMod: {
         'js': function() {
-            this.findBlockInside('checkbox-group').on('change', this._onChangeCheckbox, this);
             this.findBlockInside('card-calc').on('change', this._onChangePrice, this);
 
             this.domElem.on('submit', this._onSubmit.bind(this));
@@ -18,24 +17,6 @@ provide(BEMDOM.decl(this.name, {
 
         this.setState({
             sum: calc.getPrice()
-        });
-    },
-
-    _onChangeCheckbox: function(e) {
-        var checkbox = e.target.getCheckboxes()[0];
-
-        var cur = this.getState().sum;
-        var sum;
-
-        if (checkbox.hasMod('checked')) {
-            sum = Math.ceil(1.05 * cur);
-        } else {
-            sum = this.getState().old;
-        }
-
-        this.setState({
-            old: cur,
-            sum: sum
         });
     },
 
