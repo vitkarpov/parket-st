b78ba9125c8cf136680270650d5f281f4b0bca14
vitkarpov <viktor.s.karpov@gmail.com>
2015-12-18 15:52:45 +0300

    adds masonry relayout on resize

--- pages.blocks/catalog/catalog.js
+++ pages.blocks/catalog/catalog.js
@@ -2,13 +2,45 @@ modules.define('catalog', ['i-bem__dom', 'jquery__masonry'], function(provide, B
 
 provide(BEMDOM.decl('catalog', {
     onSetMod: {
-        'js': function() {
-            this.domElem.masonry({
-                itemSelector: '.catalog__item',
-                columnWidth: '.catalog__sizer',
-                percentPosition: true
-            });
-            this.setMod('ready')
+        'js': {
+            'inited': function() {
+                this._onResize();
+
+                this.domElem.masonry({
+                    itemSelector: '.catalog__item',
+                    columnWidth: '.catalog__sizer',
+                    percentPosition: true
+                })
+
+                this._onResize = this._onResize.bind(this);
+                BEMDOM.win.on('resize', this._onResize);
+
+                this.setMod('ready');
+            },
+            '': function() {
+                BEMDOM.win.off('resize', this._onResize);
+            }
+        },
+        'small': {
+            'true': function() {
+                if (!this.hasMod('ready')) {
+                    return;
+                }
+                this.domElem.masonry('layout');
+            },
+            '': function() {
+                this.domElem.masonry('layout');
+            }
+        }
+    },
+
+    TRESHHOLD_SMALL: 800,
+
+    _onResize: function() {
+        if (this.domElem.width() < this.TRESHHOLD_SMALL) {
+            this.setMod('small');
+        } else {
+            this.delMod('small');
         }
     }
 }));
