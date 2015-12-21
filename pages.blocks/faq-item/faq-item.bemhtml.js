b3c3f5010043b540f688a8b1cb30d2baa0eb79be
vitkarpov <viktor.s.karpov@gmail.com>
2015-12-21 18:00:01 +0300

    faq quote bug

--- pages.blocks/faq-item/faq-item.bemhtml.js
+++ pages.blocks/faq-item/faq-item.bemhtml.js
@@ -1,9 +1,20 @@
 block('faq-item')(
     content()(function() {
+        var parts = this.ctx.question.split(' ');
+        var question = [
+            parts.slice(0, parts.length - 1).join(' '),
+            ' ',
+            {
+                elem: 'last',
+                tag: 'span',
+                content: parts.slice(-1)
+            }
+        ];
+
         return [
             {
                 elem: 'question',
-                content: this.ctx.question
+                content: question
             },
             {
                 elem: 'author',
